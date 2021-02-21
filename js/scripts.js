d83f6b7ca91bd5ff9b6aa16d9089c28bbc5c71b8
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-02-21 15:35:21 -0500

    pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,68 @@
 // Business Logic for Player
-function Player() {
-
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker; 
 }
 
-
-
 Player.prototype.mark = function(choice) {
-  if(choice === "X") {
-    return "X";
+    if(choice === "X") {
+      return "X";
   } else if(choice === "O") {
-      return "O"
+      return "O";
   } else {
       return "Invalid choice"
   }
 };
 
-let testPlayer = new Player(); 
-console.log(testPlayer.mark("X"));
+let testPlayer = new Player("Marni", "X"); 
+let testPlayer2 = new Player("Jesse", "O");
+
+console.log(testPlayer);
+console.log(testPlayer2);
 
 //Business Logic for Space
-function Space() {
-
+function Space(square, markedBy) {
+  this.square = 0;
+  this.markedBy = markedBy
 }
+
+Space.prototype.spaceMarked = function(marked) {
+      if (marked === 1) {
+        this.square = 1;
+    } else if(marked === 2) {
+        this.square = 2;
+    } else if(marked === 3) {
+        this.square = 3;
+    } else if(marked === 4) {
+        this.square = 4;
+    } else if(marked === 5) {
+        this.square = 5;
+    } else if(marked === 6) {
+        this.square = 6;
+    } else if(marked === 7) {
+        this.square = 7;
+    } else if(marked === 8) {
+        this.square = 8;
+    } else if(marked === 9) {
+        this.square = 9;
+    } else {
+        return "Not a square";
+    }
+
+  return this.square;
+}
+
+let occupiedSpace = new Space();
+console.log(occupiedSpace.spaceMarked(8));
+console.log(occupiedSpace)
 
 //Business Logic for Board
 
-function Board() {
-
+function Board(newBoard) {
+this.board = newBoard
 }
 
+console.log()
 //Business Logic for Board
 function Game() {
   
